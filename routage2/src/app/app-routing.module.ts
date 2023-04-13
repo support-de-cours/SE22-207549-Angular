4758de263dadc26fd98e7e4b8e4dee766940f4bd
Arnaud BODEL <contact@osw3.net>
2023-04-13 12:29:41 +0200

    -

--- routage2/src/app/app-routing.module.ts
+++ routage2/src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepge/homepage/homepage.component';
 import { AboutComponent } from './pages/about/about/about.component';
 import { ContactComponent } from './pages/contact/contact/contact.component';
-import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
+
+import { IndexComponent } from './pages/books/index/index.component';
+// import { IndexComponent as PizzaIndexComponent } from './pages/pizza/index/index.component';
+
+import { CreateComponent } from './pages/books/create/create.component';
+import { ReadComponent } from './pages/books/read/read.component';
+import { UpdateComponent } from './pages/books/update/update.component';
+import { DeleteComponent } from './pages/books/delete/delete.component';
+// import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
 
 const routes: Routes = [
   // Homepage
@@ -11,6 +19,7 @@ const routes: Routes = [
     path: 'homepage',
     component: HomepageComponent
   },
+  
 
   // About
   {
@@ -24,17 +33,105 @@ const routes: Routes = [
     component: ContactComponent
   },
 
-  // Books - index
-  // Books - create
-  // Books - read
-  // Books - update
-  // Books - delete
 
-  // Pizzas - index
-  // Pizzas - create
-  // Pizzas - read
-  // Pizzas - update
-  // Pizzas - delete
+
+
+
+
+
+  // Books - index      site.com/books
+  {
+    path: 'books',
+    component: IndexComponent
+  },
+
+  // Books - create     site.com/books/create
+  {
+    path: 'books/create',
+    component: CreateComponent
+  },
+
+  // Books - read       site.com/books/42
+  {
+    path: 'books/:id',
+    component: ReadComponent
+  },
+
+  // Books - update     site.com/books/42/update
+  {
+    path: 'books/:id/update',
+    component: UpdateComponent
+  },
+
+  // Books - delete     site.com/books/42/delete
+  {
+    path: 'books/:id/delete',
+    component: DeleteComponent
+  },
+
+
+
+
+
+
+  {
+    path: 'books',
+    children: [
+
+      {
+        path: '', // site.com/books
+        component: IndexComponent
+      },
+
+      {
+        path: 'create', // site.com/books/create
+        component: CreateComponent
+      },
+
+      {
+        path: ':id',
+        children: [
+
+          {
+            path: '',  // site.com/books/42
+            component: ReadComponent
+          },
+
+          {
+            path: 'update', // site.com/books/42/update
+            component: UpdateComponent
+          },
+
+          {
+            path: 'delete', // site.com/books/42/delete
+            component: DeleteComponent
+          }
+        ]
+      }
+
+    ]
+  },
+
+
+
+
+
+  // Pizzas - index     site.com/pizzas
+  // {
+  //   path: 'pizzas',
+  //   component: PizzaIndexComponent
+  // },
+
+  // Pizzas - create    site.com/pizzas/create
+  // Pizzas - read      site.com/pizzas/42
+  // Pizzas - update    site.com/pizzas/42/update
+  // Pizzas - delete    site.com/pizzas/42/delete
+
+
+
+
+
+
 
   // Default route
   {
@@ -43,12 +140,17 @@ const routes: Routes = [
     pathMatch: 'full'
   },
 
-  // 
+  // WildCrad Route
   // Not Found - A modifier
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent
+  // },
   {
-    path: '',
-    component: NotFoundComponent
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
   }
+
 ];
 
 @NgModule({
